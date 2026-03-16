36ae33db245c794b824b339421761c05d2c5e881
kk-sw <33485699+kk-sw@users.noreply.github.com>
2026-03-16 17:08:38 +0100

    Make PS Bypass Active

--- mpd_control.bat
+++ mpd_control.bat
@@ -1,3 +1,2 @@
 @echo off
-::powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0mpd_control.ps1" "192.168.1.CHANGE_ME"
-powershell -File "%~dp0mpd_control.ps1" "192.168.1.CHANGE_ME"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0mpd_control.ps1" "192.168.1.CHANGE_ME"
